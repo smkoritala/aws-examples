b0021223e2a380364e2d818af4df811fe85085d4
smkoritala <smkoritala@gmail.com>
2022-11-11 19:25:14 -0500

    code started

---
+++ spring-microservice-1/src/main/java/com/example/cloud/ec2/beans/User.java
@@ -0,0 +1,42 @@
+package com.example.cloud.ec2.beans;
+
+public class User {
+
+	private String name;
+	private String password;
+	private String secretQuestion;
+	private String secretAnswer;
+	private String userId;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSecretQuestion() {
+		return secretQuestion;
+	}
+	public void setSecretQuestion(String secretQuestion) {
+		this.secretQuestion = secretQuestion;
+	}
+	public String getSecretAnswer() {
+		return secretAnswer;
+	}
+	public void setSecretAnswer(String secretAnswer) {
+		this.secretAnswer = secretAnswer;
+	}
+	
+}
